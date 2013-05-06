c69eab5455a8a0b81bc84ef3900f1e75c4287e89
Berin Martini <berin.martini@gmail.com>
2013-05-05 23:19:38 -0400

    Add option for customisation the # of indent spaces

--- src/verilog-indent.cpp
+++ src/verilog-indent.cpp
@@ -8,17 +8,25 @@
 
 int main(int argc, char* argv[])
 {
+	int space_per_indent = 4;
 	int opt;
-	while ((opt = getopt(argc, argv, "h")) != -1) {
+	while ((opt = getopt(argc, argv, "hs:")) != -1) {
 		switch (opt) {
+		case 's':
+			space_per_indent = atoi(optarg);
+			break;
 		case 'h':
 		default: /* '?' */
-			fprintf(stderr, "Usage: %s < input.v [ > output.v ]\n", argv[0]);
+			fprintf(stderr, "Usage: %s [-h] [-s num] < input.v [ > output.v ]\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	std::string indent = "    ";
+	if (space_per_indent < 2) {
+		fprintf(stderr, "Error: Number of spaces per indent must be equal to or greater then 2.\n");
+		exit(EXIT_FAILURE);
+	}
+	std::string indent = std::string((size_t) space_per_indent, ' ');
 
 	Indenter* indenter = new Indenter(indent);
 
